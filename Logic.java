60515d4e4a1ba3e4334f20c2b0826f762cce6c6b
denys <denbedilov@gmail.com>
2015-08-30 19:22:12 +0300

    create logic.java

---
+++ Logic.java
@@ -0,0 +1,214 @@
+package gamesEx2;
+
+import java.util.Random;
+
+public class Logic {
+	private int[][] board;
+	private boolean moved; 
+
+	public Logic(int boardSize)
+	{
+		board=new int[boardSize][boardSize];
+		for(int i=0; i<boardSize; i++)
+			for(int j=0; j<boardSize; j++)
+			{
+				board[i][j]=0;
+			}
+	}
+	
+	public int[][] getBoard()
+	{
+		return this.board;
+	}
+	
+	public void setBoard(int[][] newBoard)
+	{
+		for(int i=0; i<board.length; i++)
+			  for(int j=0; j<board[i].length; j++)
+			    board[i][j]=newBoard[i][j];
+	}
+	
+	public void moveLeft()
+	{
+		moved = false;
+		for(int i=0; i<board.length; i++)
+			  for(int j=0; j<board.length; j++)
+			  {
+				  shiftLeft(i, j);
+				  if(board[i][j] != 0 && j+1 < board.length)
+				  {
+					  if(board[i][j] == board[i][j+1])
+					  	{
+						  	moved = true;
+						  	board[i][j] = (board[i][j])*2;
+						  	board[i][j+1] = 0;
+						  	shiftLeft(i, j+1);
+						}
+				  }
+			  }
+	}
+	
+	public void moveDown()
+	{
+		for(int i=board.length-1; i>=0; i--)
+			  for(int j=board.length-1; j>=0; j--)
+			  {
+				  shiftDown(i, j);
+				  if(board[i][j] != 0 && i-1 >= 0)
+				  {
+					  if(board[i][j] == board[i-1][j])
+					  	{
+						  	board[i][j] = (board[i][j])*2;
+						  	board[i-1][j] = 0;
+						  	shiftDown(i-1, j);
+						}
+				  }
+			  }
+	}
+	
+	public void moveUp()
+	{
+		moved = false;
+		for(int i=0; i<board.length; i++)
+			  for(int j=0; j<board.length; j++)
+			  {
+				  shiftUp(i, j);
+				  if(board[i][j] != 0 && i+1 < board.length)
+				  {
+					  if(board[i][j] == board[i+1][j])
+					  	{
+						  	board[i][j] = (board[i][j])*2;
+						  	board[i+1][j] = 0;
+						  	shiftUp(i+1, j);
+						}
+				  }
+			  }
+	}
+	
+	public void moveRight()
+	{
+		for(int i=board.length-1; i>=0; i--)
+			  for(int j=board.length-1; j>=0; j--)
+			  {
+				  shiftRight(i, j);
+				  if(board[i][j] != 0 && j-1 >= 0)
+				  {
+					  if(board[i][j] == board[i][j-1])
+					  	{
+						  	board[i][j] = (board[i][j])*2;
+						  	board[i][j-1] = 0;
+						  	shiftRight(i, j-1);
+						}
+				  }
+			  }
+	}
+	
+	private void shiftLeft(int i, int j)
+	{
+		int start = j;
+		for(int k=board.length-j; k>0; k--)
+		{
+			if(board[i][j] == 0)
+			{
+				while(j+1 < board.length)
+				{
+					if(board[i][j+1] != 0)
+						moved = true;
+					board[i][j] = board[i][j+1];
+					j++;
+				}
+				board[i][j] = 0;
+			}
+				while(board[i][start] != 0 && start+1 < board.length)
+					start++;
+				j = start;
+				if(j == board.length)
+					break;
+		}
+	}
+	
+	private void shiftRight(int i, int j)
+	{
+		int start = j;
+		for(int k=board.length-j; k>0; k--)
+		{
+			if(board[i][j] == 0)
+			{
+				while(j-1 >= 0)
+				{
+					board[i][j] = board[i][j-1];
+					j--;
+				}
+				board[i][j] = 0;
+			}
+				if(board[i][start] != 0)
+					j = start-1;
+				else
+					j = start;
+				if(j > 0)
+					break;
+		}
+	}
+	
+	private void shiftUp(int i, int j)
+	{
+		int start = i;
+		for(int k=board.length-i; k>0; k--)
+		{
+			if(board[i][j] == 0)
+			{
+				while(i+1 < board.length)
+				{
+					if(board[i+1][j] != 0)
+						moved = true;
+					board[i][j] = board[i+1][j];
+					i++;
+				}
+				board[i][j] = 0;
+			}
+				while(board[start][j] != 0 && start+1 < board.length)
+					start++;
+				i = start;
+				if(i==board.length)
+					break;
+		}
+	}
+	
+	private void shiftDown(int i, int j)
+	{
+		int start = i;
+		for(int k=board.length-i; k>0; k--)
+		{
+			if(board[i][j] == 0)
+			{
+				while(i-1 >= 0)
+				{
+					board[i][j] = board[i-1][j];
+					i--;
+				}
+				board[i][j] = 0;
+			}
+				if(board[start][j] != 0)
+					i = start-1;
+				else
+					i = start;
+				if(i < 0)
+					break;
+		}
+	}
+	
+	public void addNewNum()
+	{
+		if(moved)
+		{
+			Random rand = new Random();
+			int rnd1, rnd2;
+			do
+			{
+				rnd1 = (rand.nextInt(board.length) + 1)-1;
+				rnd2 = (rand.nextInt(board.length) + 1)-1;
+			}while(board[rnd1][rnd2] != 0);
+			board[rnd1][rnd2] = 2;
+		}
+	}
+}
